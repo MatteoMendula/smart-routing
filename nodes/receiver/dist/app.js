2f648eebfe870c57c443225f5c2adcfb1eb07c1a
matte <mattemendu@gmail.com>
2021-06-09 23:26:39 +0200

    new build tsc

--- nodes/receiver/dist/app.js
+++ nodes/receiver/dist/app.js
@@ -1,14 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Net = require("net");
+var fs = require("fs");
 var DOCKER = false;
 var my_ip = (DOCKER) ? "10.0.0.14" : "192.168.1.114";
 var port = 8225;
 var _secret = "depl0yit";
 var received_pkts_buffer = [];
 var createReport = function () {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+    var hh = today.getHours();
+    var min = today.getMinutes();
+    var s = today.getSeconds();
+    var file_name = hh + "_" + min + "_" + s + "_" + dd + "_" + mm + "_" + yyyy + ".csv";
+    fs.appendFileSync(file_name, "seq_number,content_encripted,high_security,n_forwards,destination,timestamp_sent,timestamp_received,latency");
     for (var i in received_pkts_buffer) {
         console.log(received_pkts_buffer["seq_number"]);
+        var row = received_pkts_buffer[i];
+        var latency = Number(row[i]["timestamp_received"]) - Number(row[i]["timestamp_sent"]);
+        fs.appendFile(file_name, "\n" + row[i]["seq_number"] + ",\n          " + row[i]["content_encripted"] + ",\n          " + row[i]["high_security"] + ",\n          " + row[i]["n_forwards"] + ",\n          " + row[i]["destination"] + ",\n          " + row[i]["timestamp_sent"] + ",\n          " + row[i]["timestamp_received"] + ",\n          " + latency, function (err) {
+            if (err)
+                throw err;
+            console.log("row" + i + " ok");
+        });
     }
 };
 var server = Net.createServer();
