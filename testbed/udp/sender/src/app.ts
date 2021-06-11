439efb8ce02908e7985210504914ae20410fe5e3
matte <mattemendu@gmail.com>
2021-06-11 14:09:15 +0200

    changed freqs 2

--- testbed/udp/sender/src/app.ts
+++ testbed/udp/sender/src/app.ts
@@ -66,8 +66,8 @@ const test = (packet_limit) => {
   const third_send_pkts = packet_limit;
 
   const first_send_time_pkt_per_sec = 100;
-  const second_send_time_pkt_per_sec = 350;
-  const third_send_time_pkt_per_sec = 600;
+  const second_send_time_pkt_per_sec = 450;
+  const third_send_time_pkt_per_sec = 900;
 
   const sendPacktsFunction = ()=> {
     console.log(counter)
