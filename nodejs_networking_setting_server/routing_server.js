add89b12749a8964e5a96a3526fa6e8e21de2ccd
matte <mattemendu@gmail.com>
2022-02-21 12:21:32 +0100

    parse networking rule json

---
+++ nodejs_networking_setting_server/routing_server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { exec } = require('child_process');
+const { performance } = require('perf_hooks');
+
+// https://unix.stackexchange.com/questions/11851/iptables-allow-certain-ips-and-block-all-other-connection
+// curl -X POST http://localhost:3000/asd -H 'Content-Type: application/json' -d '{"command":"ls"}'
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const executeCommand = function (command, fn) {
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        fn(performance.now());
+        // res.json({time_ms: performance.now() - startTime});
+    });
+}
+
+const measureCommandTime = (command, res) => {
+    const startTime = performance.now();
+    executeCommand(command, (endTime)=>{
+        res.json({time_ms: endTime - startTime});
+    });
+}
+
+app.post('/executePlainCommand', (req, res) => {
+    const command = req.body.command;
+    measureCommandTime(command, res);
+});
+
+
+app.post('/executeNetworkingRule', (req, res) => {
+    const network_rule = req?.body?.zoneExtensionEdgeNodeList?.[0];
+    if (network_rule?.trafficRuleList) {
+        // sudo iptables -A INPUT -s 192.168.1.1 -j ACCEPT
+        let command = "";
+        for (var i in network_rule.trafficRuleList){
+            const rule = network_rule.trafficRuleList[i];
+            command += (i==0) ? "" : " && " + `sudo iptables -A ${rule.trafficType} -s ${rule.destinationIpAddress} -j ACCEPT`;
+        }
+        // drop other IPs (INPUT AND OUTPUT)
+        command += (command.length == 0) ? "" : " && iptables -P INPUT DROP && iptables -P OUTPUT DROP"; 
+        measureCommandTime(command, res);
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Routing server app listening on port ${port}`)
+})
